3b4ab0598e1edd350235882407af84334e3500da
Ugyen2003 <03230404.gcbs@rub.edu.bt>
2024-03-07 20:52:12 -0800

    string uppercase exercise

---
+++ stringsEx.py
@@ -0,0 +1,7 @@
+# get user input 
+# convert the user input into ALL upper case
+# print the converted string
+
+userInput = input("Enter a string: ")
+userInputConverted = userInput.upper()
+print(userInputConverted)
