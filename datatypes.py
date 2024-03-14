2d6f93c11a0c5d7bd73801dca4774d6834cff435
Ugyen2003 <03230404.gcbs@rub.edu.bt>
2024-03-14 01:26:44 -0700

    datatypes-samples

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+# creation of array
+array1 = [1, 2, 3, "thimphu", 2.5]
+
+# retrieving
+element1 = array1[0]
+element2 = array1[4]
+lastElement = array1[-5]
+print(lastElement)
+
+# modifying elements
+# array1[0] = 10
+
+# getting number of elements in an array
+no_of_elements = len(array1)
+
+# slicing
+elements = array1[0:3]
+print(elements)
